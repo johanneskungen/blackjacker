2edfd9c1a953370dff77f3ae766b4dc467f7c3a6
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-04-26 17:30:34 +0200

    meta tag updates

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -2,13 +2,19 @@ import Box from "@/components/Box";
 import Footer2 from "@/components/Footer2";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
+import Head from "next/head";
 
 function counter() {
   return (
     <div className="box_container">
+      <Head>
+        <title>Blackjacker Basic Strategy Tool</title>
+      </Head>
       <Box />
       <Sidebar />
-      <div className="absolute bottom-5 left-[50%] translate-x-[-50%]"><Footer2 /></div>
+      <div className="absolute bottom-5 left-[50%] translate-x-[-50%]">
+        <Footer2 />
+      </div>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Hero from "@/components/Hero";
+import Head from "next/head";
 
 function index() {
   return (
     <>
+      <Head>
+        <title>Play blackjack like it is supposed to with Blackjacker</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="A Blackjack tool that help beginners to learn and play as the basic strategy says" />
+        <meta charSet="UTF-8" />
+      </Head>
       <Hero />
     </>
   );
