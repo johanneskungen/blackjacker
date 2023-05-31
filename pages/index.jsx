37b8f6edc745031d5fd075927ec65479deb41939
Johannes Eriksson <86841911+johanneskungen@users.noreply.github.com>
2023-05-31 20:04:51 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ function index() {
   return (
     <>
       <Head>
+    <meta name="google-site-verification" content="q8LgerDUleYMB4KhdKR7UKl4bXzRBvxrHDLWKC61cYU" />
         <title>Play blackjack like it is supposed to with Blackjacker</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="A Blackjack tool that help beginners to learn and play as the basic strategy says" />
